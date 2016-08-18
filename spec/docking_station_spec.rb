2a81f5cc76d2b082d09416abd0cee7b7ee765918
ercekal <ercekalabalikoglu@gmail.com>
2016-08-18 18:45:00 +0100

    the issue about 2 (double :bike) items not to be same is overcame by assinging a variable to both

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,25 +1,25 @@
 require 'boris_bikes.rb'
 
 describe DockingStation do
+  let (:bike) { double :bike }
   before(:all) do
-    @station = DockingStation.new
-    @bike = double(:bike)
+    subject = DockingStation.new
   end
 
   it 'should raise error if no bikes available' do
-    expect{@station.release_bike}.to raise_error(RuntimeError)
+    expect{subject.release_bike}.to raise_error(RuntimeError)
   end
 
   it 'should dock a bike to an empty station' do
-    expect(@station.dock(@bike)).to eq([@bike])
+    expect(subject.dock(bike)).to eq([bike])
   end
 
   it 'should raise error if station is full' do
-    expect{@station.capacity.times {@station.dock(Bike.new)}}.to raise_error(RuntimeError)
+    expect{subject.capacity.times {subject.dock(Bike.new)}}.to raise_error(RuntimeError)
   end
 
   it 'should have a default capacity' do
-    expect(@station.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 
   it 'should set a given capacity' do
@@ -27,10 +27,9 @@ describe DockingStation do
     expect(station.capacity).not_to eq DockingStation::DEFAULT_CAPACITY
   end
 
-  let (:bike) { double :bike }
-  it 'releases working bikes' do
-    bike = double("bike", :working? => true)
-    subject.dock(bike)
-    expect(subject.release_bike).to be_working
-  end
+    it 'releases working bikes' do
+      bike = double("bike", :working? => true)
+      subject.dock(bike)
+      expect(subject.release_bike).to be_working
+    end
 end
